b8689edeca44b905c66a57210336c0c01fbaad56
Florian Heinemann <florian.heinemann@gmail.com>
2014-06-19 22:38:15 +0200

    First content

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	res.render('index', { validation: req.flash('validation')[0], error: req.flash('error')[0] });
+	res.render('index', { validation: req.flash('validation')[0], error: req.flash('error')[0], homepage: true });
+});
+
+/* GET home page. */
+router.get('/getstarted', function(req, res) {
+	res.render('getstarted', { validation: req.flash('validation')[0], error: req.flash('error')[0] });
 });
 
 module.exports = router;
